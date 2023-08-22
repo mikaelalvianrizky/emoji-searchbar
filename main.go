7631ea1d62e7b125bbb3a38f0432e3931202a45e
Mikael Alvian Rizky <mikael.alvian@ui.ac.id>
2023-08-22 15:06:33 +0700

    deploy lokal berhasil

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "context"
     "fmt"
     "log"
+    "net/http"
 
     "github.com/ServiceWeaver/weaver"
 )
@@ -14,18 +15,29 @@ func main() {
     }
 }
 
-type app struct{
+type app struct {
     weaver.Implements[weaver.Main]
     reverser weaver.Ref[Reverser]
+    server    weaver.Listener `weaver:"server"`
 }
 
 func serve(ctx context.Context, app *app) error {
-    // Call the Reverse method.
-    var r Reverser = app.reverser.Get()
-    reversed, err := r.Reverse(ctx, "!dlroW ,olleH")
-    if err != nil {
-        return err
-    }
-    fmt.Println(reversed)
-    return nil
+    // The hello listener will listen on a random port chosen by the operating
+    // system. This behavior can be changed in the config file.
+    fmt.Printf("hello listener available on %v\n", app.server)
+
+    // Serve the /hello endpoint.
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        name := r.URL.Query().Get("name")
+        if name == "" {
+            name = "World"
+        }
+        reversed, err := app.reverser.Get().Reverse(ctx, name)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        fmt.Fprintf(w, "Hello, %s!\n", reversed)
+    })
+    return http.Serve(app.server, nil)
 }
