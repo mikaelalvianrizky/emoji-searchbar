c1facda4fa52637d6ca4b98b7c76286e36cdd190
Mikael Alvian Rizky <mikael.alvian@ui.ac.id>
2023-08-22 14:12:14 +0700

    add reverser interface for reverse string

--- weaver_gen.go
+++ weaver_gen.go
@@ -5,8 +5,10 @@ package main
 
 import (
 	"context"
+	"errors"
 	"github.com/ServiceWeaver/weaver"
 	"github.com/ServiceWeaver/weaver/runtime/codegen"
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 	"reflect"
 )
@@ -43,15 +45,32 @@ func init() {
 		ServerStubFn: func(impl any, addLoad func(uint64, float64)) codegen.Server {
 			return main_server_stub{impl: impl.(weaver.Main), addLoad: addLoad}
 		},
+		RefData: "⟦77771a36:wEaVeReDgE:github.com/ServiceWeaver/weaver/Main→search-emoji/Reverser⟧\n",
+	})
+	codegen.Register(codegen.Registration{
+		Name:  "search-emoji/Reverser",
+		Iface: reflect.TypeOf((*Reverser)(nil)).Elem(),
+		Impl:  reflect.TypeOf(reverser{}),
+		LocalStubFn: func(impl any, caller string, tracer trace.Tracer) any {
+			return reverser_local_stub{impl: impl.(Reverser), tracer: tracer, reverseMetrics: codegen.MethodMetricsFor(codegen.MethodLabels{Caller: caller, Component: "search-emoji/Reverser", Method: "Reverse", Remote: false})}
+		},
+		ClientStubFn: func(stub codegen.Stub, caller string) any {
+			return reverser_client_stub{stub: stub, reverseMetrics: codegen.MethodMetricsFor(codegen.MethodLabels{Caller: caller, Component: "search-emoji/Reverser", Method: "Reverse", Remote: true})}
+		},
+		ServerStubFn: func(impl any, addLoad func(uint64, float64)) codegen.Server {
+			return reverser_server_stub{impl: impl.(Reverser), addLoad: addLoad}
+		},
 		RefData: "",
 	})
 }
 
 // weaver.InstanceOf checks.
 var _ weaver.InstanceOf[weaver.Main] = (*app)(nil)
+var _ weaver.InstanceOf[Reverser] = (*reverser)(nil)
 
 // weaver.Router checks.
 var _ weaver.Unrouted = (*app)(nil)
+var _ weaver.Unrouted = (*reverser)(nil)
 
 // Local stub implementations.
 
@@ -63,6 +82,35 @@ type main_local_stub struct {
 // Check that main_local_stub implements the weaver.Main interface.
 var _ weaver.Main = (*main_local_stub)(nil)
 
+type reverser_local_stub struct {
+	impl           Reverser
+	tracer         trace.Tracer
+	reverseMetrics *codegen.MethodMetrics
+}
+
+// Check that reverser_local_stub implements the Reverser interface.
+var _ Reverser = (*reverser_local_stub)(nil)
+
+func (s reverser_local_stub) Reverse(ctx context.Context, a0 string) (r0 string, err error) {
+	// Update metrics.
+	begin := s.reverseMetrics.Begin()
+	defer func() { s.reverseMetrics.End(begin, err != nil, 0, 0) }()
+	span := trace.SpanFromContext(ctx)
+	if span.SpanContext().IsValid() {
+		// Create a child span for this method.
+		ctx, span = s.tracer.Start(ctx, "main.Reverser.Reverse", trace.WithSpanKind(trace.SpanKindInternal))
+		defer func() {
+			if err != nil {
+				span.RecordError(err)
+				span.SetStatus(codes.Error, err.Error())
+			}
+			span.End()
+		}()
+	}
+
+	return s.impl.Reverse(ctx, a0)
+}
+
 // Client stub implementations.
 
 type main_client_stub struct {
@@ -71,6 +119,70 @@ type main_client_stub struct {
 
 // Check that main_client_stub implements the weaver.Main interface.
 var _ weaver.Main = (*main_client_stub)(nil)
+
+type reverser_client_stub struct {
+	stub           codegen.Stub
+	reverseMetrics *codegen.MethodMetrics
+}
+
+// Check that reverser_client_stub implements the Reverser interface.
+var _ Reverser = (*reverser_client_stub)(nil)
+
+func (s reverser_client_stub) Reverse(ctx context.Context, a0 string) (r0 string, err error) {
+	// Update metrics.
+	var requestBytes, replyBytes int
+	begin := s.reverseMetrics.Begin()
+	defer func() { s.reverseMetrics.End(begin, err != nil, requestBytes, replyBytes) }()
+
+	span := trace.SpanFromContext(ctx)
+	if span.SpanContext().IsValid() {
+		// Create a child span for this method.
+		ctx, span = s.stub.Tracer().Start(ctx, "main.Reverser.Reverse", trace.WithSpanKind(trace.SpanKindClient))
+	}
+
+	defer func() {
+		// Catch and return any panics detected during encoding/decoding/rpc.
+		if err == nil {
+			err = codegen.CatchPanics(recover())
+			if err != nil {
+				err = errors.Join(weaver.RemoteCallError, err)
+			}
+		}
+
+		if err != nil {
+			span.RecordError(err)
+			span.SetStatus(codes.Error, err.Error())
+		}
+		span.End()
+
+	}()
+
+	// Preallocate a buffer of the right size.
+	size := 0
+	size += (4 + len(a0))
+	enc := codegen.NewEncoder()
+	enc.Reset(size)
+
+	// Encode arguments.
+	enc.String(a0)
+	var shardKey uint64
+
+	// Call the remote method.
+	requestBytes = len(enc.Data())
+	var results []byte
+	results, err = s.stub.Run(ctx, 0, enc.Data(), shardKey)
+	replyBytes = len(results)
+	if err != nil {
+		err = errors.Join(weaver.RemoteCallError, err)
+		return
+	}
+
+	// Decode the results.
+	dec := codegen.NewDecoder(results)
+	r0 = dec.String()
+	err = dec.Error()
+	return
+}
 
 // Server stub implementations.
 
@@ -88,4 +200,47 @@ func (s main_server_stub) GetStubFn(method string) func(ctx context.Context, arg
 	default:
 		return nil
 	}
+}
+
+type reverser_server_stub struct {
+	impl    Reverser
+	addLoad func(key uint64, load float64)
+}
+
+// Check that reverser_server_stub implements the codegen.Server interface.
+var _ codegen.Server = (*reverser_server_stub)(nil)
+
+// GetStubFn implements the codegen.Server interface.
+func (s reverser_server_stub) GetStubFn(method string) func(ctx context.Context, args []byte) ([]byte, error) {
+	switch method {
+	case "Reverse":
+		return s.reverse
+	default:
+		return nil
+	}
+}
+
+func (s reverser_server_stub) reverse(ctx context.Context, args []byte) (res []byte, err error) {
+	// Catch and return any panics detected during encoding/decoding/rpc.
+	defer func() {
+		if err == nil {
+			err = codegen.CatchPanics(recover())
+		}
+	}()
+
+	// Decode arguments.
+	dec := codegen.NewDecoder(args)
+	var a0 string
+	a0 = dec.String()
+
+	// TODO(rgrandl): The deferred function above will recover from panics in the
+	// user code: fix this.
+	// Call the local method.
+	r0, appErr := s.impl.Reverse(ctx, a0)
+
+	// Encode the results.
+	enc := codegen.NewEncoder()
+	enc.String(r0)
+	enc.Error(appErr)
+	return enc.Data(), nil
 }
